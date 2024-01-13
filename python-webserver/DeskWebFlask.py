a66b6a28bbb3d9e68031965ea4354389dabefc6a
Peppino's Stuff <125193665+PeppinoTechTrends@users.noreply.github.com>
2024-01-13 23:40:05 +0100

    Update webserver

---
+++ python-webserver/DeskWebFlask.py
@@ -0,0 +1,61 @@
+from flask import Flask, Response, redirect, render_template
+import time
+from confluent_kafka import Consumer, KafkaError
+
+#The provided code is a Flask web application that creates a video streaming service using Apache Kafka.
+app = Flask(__name__)
+
+
+#setup kafka
+def create_kafka_consumer():
+    consumer_conf = {
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'consumer-group',
+        'auto.offset.reset': 'earliest'
+    }
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe(['streamingTopic'])
+    return consumer
+
+
+# Streams video data received from Kafka
+# Continuously polls for new messages from Kafka.
+# If a message contains video frame data, it is yielded as part of a streaming response.
+
+def generate_video_stream():
+    consumer = create_kafka_consumer()
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    print(msg.error())
+                    break
+
+            frame = msg.value()
+            if frame:
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            else:
+                time.sleep(0.01)
+    finally:
+        consumer.close()
+
+#setup web page
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
